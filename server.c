ba446adb55e4a64e9d4c24b0fbf7e5024b91dcaa
ThomasRoinas <roinastom@gmail.com>
2025-01-03 14:14:15 +0200

    Implement parent and child order processing functions in server.c

--- server.c
+++ server.c
@@ -21,6 +21,63 @@ void init_catalog(product catalog[])
     }
 }
 
+void parent_orders(product catalog[], int p1[], int p2[])
+{
+    int i;
+    int sum_parag;
+    int sum_succparag;
+    int sum_failparag;
+    int sum_price;
+
+   close(p1[0]);
+   close(p2[1]);
+
+   for(i=0; i<20; i++)
+   {
+        int arithmos; 
+        read(p2[0], &arithmos, sizeof(arithmos));
+        sum_parag = sum_parag + 1;
+
+        if(arithmos >= 0 && arithmos < 20)
+        {
+            if(catalog[arithmos].item_count > 0)
+            {
+                sum_succparag++;
+                sum_price = sum_price + catalog[arithmos].price;
+                catalog[arithmos].item_count--;
+                write (p1[1], "success", sizeof("success"));
+            }
+            
+            else
+            {
+                sum_failparag++;
+                write (p1[1], "fail", sizeof("fail"));
+            }
+        }
+
+        else
+        {
+            sum_failparag++;
+            write (p1[1], "fail", sizeof("fail"));
+        }
+
+        sleep(1);
+    }  
+}
+
+void child_orders(int p1[2], int p2[2])
+{
+    int i;
+
+    close(p1[1]);
+    close(p2[0]);
+
+    for(i=0; i<20; i++)
+    {
+        
+    }
+}
+
 int main()
 {
     product catalog[20];
@@ -52,6 +109,8 @@ int main()
     else if(pid1 > 0)
     {
         printf("Parent process\n");
+
+
 
         int pid[5];
 
