7b7bf92e56b18b02ba200fad491337a4aaf55a5a
ThomasRoinas <roinastom@gmail.com>
2025-01-09 16:42:37 +0200

    Refactor parent_orders function to improve pointer usage for summary parameters

--- server.c
+++ server.c
@@ -14,7 +14,7 @@ typedef struct
 
 void init_catalog(product catalog[])    
 {
-    for(int i = 0; i<20; i++)
+    for(int i=0; i<20; i++)
     {
         strcpy(catalog[i].description, "Product(i)");
         catalog[i].price = i*3;
@@ -33,28 +33,28 @@ void parent_orders(product catalog[], int p1[], int p2[], int *sum_parag, int *s
    {
         int arithmos; 
         read(p2[0], &arithmos, sizeof(arithmos));
-        *sum_parag = *sum_parag + 1;
+        (*sum_parag) = (*sum_parag) + 1;
 
         if(arithmos >= 0 && arithmos < 20)
         {
             if(catalog[arithmos].item_count > 0)
             {
-                *sum_succparag++;
-                *sum_price = *sum_price + catalog[arithmos].price;
+                (*sum_succparag)++;
+                (*sum_price) = (*sum_price) + catalog[arithmos].price;
                 catalog[arithmos].item_count--;
                 write (p1[1], "success", sizeof("success"));
             }
             
             else
             {
-                *sum_failparag++;
+                (*sum_failparag)++;
                 write (p1[1], "fail", sizeof("fail"));
             }
         }
 
         else
         {
-            *sum_failparag++;
+            (*sum_failparag)++;
             write (p1[1], "fail", sizeof("fail"));
         }
 
