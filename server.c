d6a7f32c2f1aad92c885bf8d44e128e10d4433da
ThomasRoinas <roinastom@gmail.com>
2025-01-10 21:14:24 +0200

    Reorder parent_orders call to ensure proper execution flow before statistics calculation

--- server.c
+++ server.c
@@ -171,12 +171,12 @@ int main()
         }
     }
 
-    parent_orders(catalog, p1, p2, &sum_parag, &sum_succparag, &sum_failparag, &sum_price);  
-
     for(i=0; i<5; i++)
     {
         wait(NULL);
     }
+
+    parent_orders(catalog, p1, p2, &sum_parag, &sum_succparag, &sum_failparag, &sum_price);  
 
     anafora(catalog, sum_parag, sum_succparag, sum_failparag, sum_price);
     statistics(sum_parag, sum_succparag, sum_failparag, sum_price);
