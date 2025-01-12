72b2f9bb3d569dd3a5d1fc15315fa0c4df80e6b2
ThomasRoinas <roinastom@gmail.com>
2025-01-12 18:05:03 +0200

    Fix merge conflicts in eshop.h and clean up function declarations

--- eshop.h
+++ eshop.h
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 87be3a1aea0d7531dab2c3c88e1c3086dcdfaa0c
-void init_catalog(product catalog[])    //Συνάρτηση για την Αρχικοποίηση του πίνακα catalog
+void init_catalog(product catalog[])   
 void parent_orders(product catalog[], int p1[2], int p2[2], int *sum_parag, int *sum_succparag, int *sum_failparag, double *sum_price)
 void child_orders(int p1[2], int p2[2], int client_arithmos)
 void anafora(product catalog[])
 void statistics(int sum_parag, int sum_succparag, int sum_failparag, double sum_price)
-<<<<<<< HEAD
-
-=======
->>>>>>> 87be3a1aea0d7531dab2c3c88e1c3086dcdfaa0c
