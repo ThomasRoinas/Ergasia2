82178a644f0544d7777af34070458208e4ae5020
ThomasRoinas <roinastom@gmail.com>
2025-01-12 18:20:40 +0200

    Add newline and header comment in eshop.h

--- eshop.h
+++ eshop.h
@@ -3,3 +3,5 @@ void parent_orders(product catalog[], int p1[2], int p2[2], int *sum_parag, int 
 void child_orders(int p1[2], int p2[2], int client_arithmos)
 void anafora(product catalog[])
 void statistics(int sum_parag, int sum_succparag, int sum_failparag, double sum_price)
+
+//Header file
