585031e25f6367508b8234780327cb3de787f75a
Thomas Roinas <162818253+ThomasRoinas@users.noreply.github.com>
2025-01-12 17:55:36 +0200

    Update eshop.h

--- eshop.h
+++ eshop.h
@@ -4,4 +4,3 @@ void parent_orders(product catalog[], int p1[2], int p2[2], int *sum_parag, int 
 void child_orders(int p1[2], int p2[2], int client_arithmos)
 void anafora(product catalog[])
 void statistics(int sum_parag, int sum_succparag, int sum_failparag, double sum_price)
-
