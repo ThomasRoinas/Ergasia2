41c7b0be4829dca334909199d1d1fb91a414550b
ThomasRoinas <roinastom@gmail.com>
2025-03-15 15:03:30 +0200

    Add header comment to server.h for clarity on server-client functionality

--- server.h
+++ server.h
@@ -1,7 +1,10 @@
+// Header file for the server-client program
+
 void init_catalog(product catalog[])   
 void parent_orders(product catalog[], int p1[2], int p2[2], int *sum_parag, int *sum_succparag, int *sum_failparag, double *sum_price)
 void child_orders(int p1[2], int p2[2], int client_arithmos)
 void anafora(product catalog[])
 void statistics(int sum_parag, int sum_succparag, int sum_failparag, double sum_price)
 
-//Header file
+
+
